ad783807d9b68cb8136f58f7f6825e0ace9753a9
Andre Weissflog <floooh@gmail.com>
2015-03-24 12:19:55 +0100

    Partial Windows support (but see floooh/fips/#77)

--- cmake/find_fbxsdk.cmake
+++ cmake/find_fbxsdk.cmake
@@ -8,6 +8,8 @@
 #       FBXSDK_INCLUDE_DIR
 #
 set(_fbxsdk_version "2015.1")
+set(_fbxsdk_vstudio_version "vs2013")
+
 message("Looking for FBX SDK version: ${_fbxsdk_version}")
 
 if (FIPS_HOST_OSX)
@@ -17,13 +19,27 @@ if (FIPS_HOST_OSX)
     set(_fbxsdk_libname_debug "libfbxsdk.a")
     set(_fbxsdk_libname_release "libfbxsdk.a")
 elseif (FIPS_HOST_WINDOWS)
-    message(FATAL_ERROR "FIXME: find FBX SDK on Windows")
+    # the $ENV{PROGRAMFILES} variable doesn't really work since there's no 
+    # 64-bit cmake version
+    set(_fbxsdk_approot "C:/Program Files/Autodesk/FBX/FBX SDK")
+    if (FIPS_WIN32)
+        set(_fbxsdk_libdir_debug "lib/${_fbxsdk_vstudio_version}/x86/debug")
+        set(_fbxsdk_libdir_release "lib/${_fbxsdk_vstudio_version}/x86/release")
+        set(_fbxsdk_libname_debug "libfbxsdk-mt.lib")
+        set(_fbxsdk_libname_release "libfbxsdk-mt.lib")
+    else()
+        set(_fbxsdk_libdir_debug "lib/${_fbxsdk_vstudio_version}/x64/debug")
+        set(_fbxsdk_libdir_release "lib/${_fbxsdk_vstudio_version}/x64/release")
+        set(_fbxsdk_libname_debug "libfbxsdk-mt.lib")
+        set(_fbxsdk_libname_release "libfbxsdk-mt.lib")
+    endif()
 elseif (FIPS_HOST_LINUX)
     message(FATAL_ERROR" FIXME: find FBX SDK on Linux")
 endif()
 
 # should point the the FBX SDK installation dir
 set(_fbxsdk_root "${_fbxsdk_approot}/${_fbxsdk_version}")
+message("_fbxsdk_root: ${_fbxsdk_root}")
 
 # find header dir and libs
 find_path(FBXSDK_INCLUDE_DIR "fbxsdk.h" 
